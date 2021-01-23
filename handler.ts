247efb77bb6e5fd49cff30c4608e726daf587f23
pistachio05 <malaya@uci.edu>
2021-01-23 07:46:52 -0800

    update serverless added apollo-lambda

--- handler.ts
+++ handler.ts
@@ -1,12 +1,19 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import {  ApolloServer, gql } from 'apollo-server-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+export const graphqlHandler = server.createHandler();
