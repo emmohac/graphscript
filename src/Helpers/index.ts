dc286dc02992e9fc52bd11bf13757f5caa2e6b4e
Huy Minh Tran <emmohac.tran@yahoo.com>
2021-02-27 14:12:22 -0800

    Added secured pattern into user register

--- src/Helpers/index.ts
+++ src/Helpers/index.ts
@@ -1,6 +1,1 @@
-export const strongPassword = (password: string): boolean => {
-  const regex = new RegExp(
-    "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,}$"
-  );
-  return regex.test(password);
-};
+export const strongPasswordPattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,}$";
